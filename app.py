27a036d97cac76d824bb0c1328d4ba8b42bdc67b
djmolloy57 <dj_molloy57@hotmail.com>
2022-04-09 09:41:32 +0000

    fix dynamic book category title to book_by_category page where no category type is available

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ def get_fantasy():
 def get_thriller():
     result_thriller = mongo.db.books.find({
     "Category" : { "$eq" : "Thriller"}})
+    if len(list(result_thriller))==0:
+       result_thriller=" "
     return render_template("book_by_category.html", result_1=result_thriller)
 
 
